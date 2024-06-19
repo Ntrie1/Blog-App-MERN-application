7d88b4d2cd660e782fb926e31210ba72cf245eba
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-06-19 13:17:22 +0300

    connected to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotnev from 'dotenv';
+
+dotnev.config();
+
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => { console.log('mongodb is connected!') })
+    .catch((err) => { console.log(err) })
 
 const app = express();
 
