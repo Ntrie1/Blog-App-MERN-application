aafcc571622591e2c6f4ad8e126d3609694e4d12
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-06-21 21:22:19 +0300

    added posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight } from 'react-icons/hi'
+import { HiUser, HiArrowSmRight, HiDocumentText } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState('');
   const dispatch = useDispatch();
+  const {currentUser} = useSelector(state => state.user)
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -36,13 +38,19 @@ export default function DashSidebar() {
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
           <Link to={'/dashboard?tab=profile'}>
-            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'>
+            <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
               Profile
             </Sidebar.Item >
           </Link>
-          <Sidebar.Item onClick={handleSignOut} icon={HiArrowSmRight} className='cursor-pointer'>
+          
+          <Link to={'/dashboard?tab=posts'}>
+            <Sidebar.Item active={tab === 'post'} icon={HiDocumentText} as='div'>
+              Posts
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item onClick={handleSignOut} icon={HiArrowSmRight} className='cursor-pointer' as='div'>
             Sign Out
           </Sidebar.Item >
         </Sidebar.ItemGroup>
