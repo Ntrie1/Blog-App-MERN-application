3e3add49ee6ad2d6cc91a0a789d5d0552f4554a4
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-06-21 14:01:35 +0300

    created post api route and controller

---
+++ api/models/post.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: 'https://static.semrush.com/blog/uploads/media/1f/95/1f958e0bf47a62a457fa29a05f447c59/how-to-format-your-blog-post.svg'
+    },
+    category: {
+        type: String,
+        default: 'uncategorized',
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+}, { timestamps: true });
+
+
+const Post = mongoose.model('Post', postSchema);
+export default Post;
