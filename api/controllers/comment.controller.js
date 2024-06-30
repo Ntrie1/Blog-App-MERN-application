bd2ef2efb4c5bb350ec0060639386959b7500717
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-06-30 20:59:10 +0300

    like comment route created

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -41,3 +41,31 @@ export const getPostComments = async (req, res, next) => {
     }
 
 };
+
+export const likeComment = async (req, res, next) => {
+    const { commentId } = req.params;
+    const userId = req.user.id;
+
+    try {
+        const comment = await Comment.findById(commentId);
+        if(!comment){
+            return next(errorHandler(404, 'Comment not found.'))
+        };
+
+        const userIndex = comment.likes.indexOf(userId);
+        
+        if(userIndex === -1){
+            comment.numberOfLikes ++;
+            comment.likes.push(userId);
+        } else {
+            comment.numberOfLikes --;
+            comment.likes.splice(userIndex, 1);
+        };
+
+        await comment.save();
+        res.status(200).json(comment);
+    } catch (error) {
+        next(error)
+    }
+
+};
