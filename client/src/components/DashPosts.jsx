599a93d7018a6a6d189a24e7e0e7ad23b0dd1d23
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-06-30 10:31:35 +0300

    fixed key err

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -92,7 +92,7 @@ export default function DashPosts() {
               </Table.HeadCell>
             </Table.Head>
             {userPosts.map((post) => (
-              <Table.Body className='devide-y'>
+              <Table.Body className='devide-y' key={post._id}>
                 <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                   <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
                   <Table.Cell>
