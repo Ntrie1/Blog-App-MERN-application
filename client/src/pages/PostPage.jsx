7084765dcf54955cb8dc7e8498e37343a218e612
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-09-23 21:12:39 +0300

    Update PostPage.jsx

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -99,7 +99,7 @@ export default function PostPage() {
 
             <div className='flex flex-col justify-center items-center mb-5'>
                 <h1 className='text-xl mt-5'>Recent Articles</h1>
-                <div>
+                <div className='flex flex-wrap gap-5 mt-5 justify-center'>
                     {
                         recentPosts &&
                         recentPosts.map((post) =>
