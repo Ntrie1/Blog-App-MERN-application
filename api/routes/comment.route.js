a37b636fb6dfdd680395d313f12e2bbf4841c665
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-09-28 13:08:54 +0300

    getComments route and controller created

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createComment, delteComment, editComment, getPostComments, likeComment } from "../controllers/comment.controller.js";
+import { createComment, delteComment, editComment, getComments, getPostComments, likeComment } from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.get('/getcomments',verifyToken, getComments);
 router.put('/like/:commentId', verifyToken, likeComment);
 router.put('/edit/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, delteComment);
