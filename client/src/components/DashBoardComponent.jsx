ead9eb11acbc1f9bce87ffb96c6b052fcc6d129a
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-12-11 17:24:18 +0200

    Update DashBoardComponent.jsx

--- client/src/components/DashBoardComponent.jsx
+++ client/src/components/DashBoardComponent.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import {HiAnnotation, HiArrowNarrowUp, HiDocumentText, HiOutlineUserGroup} from 'react-icons/hi';
+import {Button, Table} from 'flowbite-react'
+import {Link} from 'react-router-dom'
 
 export default function DashBoardComponent() {
   const [users, setUsers] = useState([]);
@@ -37,7 +40,7 @@ export default function DashBoardComponent() {
         if (res.ok) {
           setPosts(data.posts);
           setTotalPosts(data.totalPosts);
-          setLastMonthPosts(data.lastMonthposts);
+          setLastMonthPosts(data.lastMonthPosts);
         }
       } catch (error) {
         console.log(error.message);
@@ -68,6 +71,165 @@ export default function DashBoardComponent() {
 
 
   return (
-    <div>DashBoardComponent</div>
+    <div className='p-3 md:mx-auto'>
+      <div className='flex-wrap flex gap-4 justify-center'>
+      <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+        <div className='flex justify-between'>
+          <div className=''>
+            <h3 className='text-gray-500 text-md uppercase'>Total Users</h3>
+            <p className='text-2xl'>{totalUsers}</p>
+          </div>
+            <HiOutlineUserGroup className='bg-teal-600 text-white rounded-full text-5xl p-3 shadow-lg' />
+        </div>
+          <div className=' flex gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp/>
+              {lastMonthUsers}
+            </span>
+            <div className='text-gray-500'> Last Month 
+
+            </div>
+          </div>
+      </div>
+      <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+        <div className='flex justify-between'>
+          <div className=''>
+            <h3 className='text-gray-500 text-md uppercase'>Total Comments</h3>
+            <p className='text-2xl'>{totalComments}</p>
+          </div>
+            <HiAnnotation className='bg-indigo-600 text-white rounded-full text-5xl p-3 shadow-lg' />
+        </div>
+          <div className=' flex gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp/>
+              {lastMonthComments}
+            </span>
+            <div className='text-gray-500'> Last Month 
+
+            </div>
+          </div>
+      </div>
+      <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+        <div className='flex justify-between'>
+          <div className=''>
+            <h3 className='text-gray-500 text-md uppercase'>Total Posts</h3>
+            <p className='text-2xl'>{totalPosts}</p>
+          </div>
+            <HiDocumentText className='bg-lime-600 text-white rounded-full text-5xl p-3 shadow-lg' />
+        </div>
+          <div className=' flex gap-2 text-sm'>
+            <span className='text-green-500 flex items-center'>
+              <HiArrowNarrowUp/>
+              {lastMonthPosts}
+            </span>
+            <div className='text-gray-500'> Last Month 
+
+            </div>
+          </div>
+      </div>
+      </div>
+
+      <div className='flex flex-wrap gap-4 py-3 mx-auto justify-center'>
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rpunded-md dark:bg-gray-800'>
+          <div className='flex justify-between p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent Users</h1>
+            <Button outline gradientDuoTone='purpleToPink'>
+              <Link to={"/dashboard?tab=users"}>
+              See all
+              </Link>
+              </Button>
+          </div>
+
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell> User image </Table.HeadCell>
+              <Table.HeadCell> username </Table.HeadCell>
+            </Table.Head>
+
+            {users && users.map((user) => (
+              <Table.Body key={user._id} className='devide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800 '>
+                  <Table.Cell>
+                    <img src={user.profilePicture} alt="user" className='w-10 h-10 rounded-full bg-gray-500' />
+                  </Table.Cell>
+                  <Table.Cell>
+                    {user.username}
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ) )}
+          </Table>
+        </div>
+
+       
+
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rpunded-md dark:bg-gray-800'>
+          <div className='flex justify-between p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent Comments</h1>
+            <Button outline gradientDuoTone='purpleToPink'>
+              <Link to={"/dashboard?tab=users"}>
+              See all
+              </Link>
+              </Button>
+          </div>
+
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell> Comment content </Table.HeadCell>
+              <Table.HeadCell> Likes </Table.HeadCell>
+            </Table.Head>
+
+            {comments && comments.map((comment) => (
+              <Table.Body key={comment._id} className='devide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800 '>
+                  <Table.Cell className='w-96'>
+                    <p className='line-clamp-2'>{comment.content}</p>
+                  </Table.Cell>
+                  <Table.Cell>
+                    {comment.likes}
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ) )}
+          </Table>
+        </div>
+
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rpunded-md dark:bg-gray-800'>
+          <div className='flex justify-between p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent Posts</h1>
+            <Button outline gradientDuoTone='purpleToPink'>
+              <Link to={"/dashboard?tab=users"}>
+              See all
+              </Link>
+              </Button>
+          </div>
+
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell> Post image </Table.HeadCell>
+              <Table.HeadCell> Post Title </Table.HeadCell>
+              <Table.HeadCell> Post Category </Table.HeadCell>
+            </Table.Head>
+
+            {posts && posts.map((post) => (
+              <Table.Body key={post._id} className='devide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800 '>
+                  <Table.Cell>
+                    <img src={post.image} alt="user" className='w-14 h-10 rounded-md bg-gray-500' />
+                  </Table.Cell>
+                  <Table.Cell>
+                    {post.title}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {post.category}
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ) )}
+          </Table>
+        </div>
+      </div>
+      
+    </div>
   )
 }
