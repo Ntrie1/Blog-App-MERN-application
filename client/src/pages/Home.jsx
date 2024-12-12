4abc18f80748e9a43cadfca5746f8014ea4b30f2
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-12-12 17:51:25 +0200

    home page fetching completed with the UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import CallToAction from '../components/CallToAction'
+import PostCard from '../components/PostCard';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts)
+    }
+
+    fetchData();
+  }, []);
+  console.log(posts);
+
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-6 p-p-3 p-28 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-sm sm:text-sm'>Here you can find variety of articles and tutorials on topics such as web development. software engineering and programming.</p>
+        <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>
+          View all posts
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction />
+      </div>
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-6 py-7'>
+        {
+          posts && posts.length > 0 && (
+            <div className='flex flex-col gap-6'>
+              <h2 className='text-2xl font-semibold text-center'>
+                Recent Posts</h2>
+              <div className='flex flex-wrap gap-4 justify-center'>
+                {posts.map((post) => (
+                  <PostCard key={post._id} post={post} />
+                ))}
+              </div>
+              <Link to='/search' className='text-lg text-teal-500 hover:unnderline text-center'>
+                View all posts
+              </Link>
+            </div>
+          )
+        }
+
+      </div>
+    </div>
   )
-}
+} 
