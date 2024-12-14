75cf294875932847e1755af5118e733790827591
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-12-14 19:56:24 +0200

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3'>
+        <div className=''>
+          <h1 className='text-3xl font-semibold text-center my-7'>About Tech Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum corporis ipsa, sapiente, placeat provident molestiae distinctio modi dolorum corrupti rem dolor reiciendis tempore blanditiis deleniti cupiditate, odio iusto totam incidunt!
+            </p>
+
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum corporis ipsa, sapiente, placeat provident molestiae distinctio modi dolorum corrupti rem dolor reiciendis tempore blanditiis deleniti cupiditate, odio iusto totam incidunt!
+            </p>
+
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum corporis ipsa, sapiente, placeat provident molestiae distinctio modi dolorum corrupti rem dolor reiciendis tempore blanditiis deleniti cupiditate, odio iusto totam incidunt!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
