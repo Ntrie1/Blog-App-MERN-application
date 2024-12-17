0063eddca2e660936e432f38302d6113847bcb77
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-12-17 18:05:53 +0200

    Update Search.jsx

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -111,68 +111,67 @@ export default function Search() {
 
     return (
         <div className='flex flex-col md:flex-row'>
-            <div className='p-7 border-b md:border-r md:min-h-screen border-gray-500'>
-                <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
-                    <div className='flex items-center gap-2 '>
-                        <label className='whitespace-nowrap font-semibold'>Search Term:</label>
-                        <TextInput placeholder='Search...'
-                            id='searchTerm' type='text'
-                            value={sideBarData.searchTerm}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label className='font-semibold'>Sort: </label>
-                        <Select onChange={handleChange} id='sort' value={sideBarData.sort}>
-                            <option value="desc">Latest</option>
-                            <option value="asc">Oldest</option>
-                        </Select>
-                    </div>
-
-                    <div>
-                        <label className='font-semibold'>Category: </label>
-                        <Select onChange={handleChange} id='category' value={sideBarData.category}>
-                            <option value="uncategorized">Uncategorized</option>
-                            <option value="reactjs">React.js</option>
-                            <option value="nextjs">Next.js</option>
-                            <option value="javascript">JavaScript</option>
-                        </Select>
-                    </div>
-                    <Button type='submit' outline gradientDuoTone='purpleToPink' >
-                        Apply Filters
-                    </Button>
-
-
-
-                </form>
+        <div className='p-7 border-b md:border-r md:min-h-screen border-gray-500'>
+          <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
+            <div className='flex   items-center gap-2'>
+              <label className='whitespace-nowrap font-semibold'>
+                Search Term:
+              </label>
+              <TextInput
+                placeholder='Search...'
+                id='searchTerm'
+                type='text'
+                value={sideBarData.searchTerm}
+                onChange={handleChange}
+              />
             </div>
-            <div>
-                <h1 className='text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5'>Post results:</h1>
-                <div className='p-7 flex flex-wrap gap-4'>
-                    {
-                        !loading && posts.length == 0 && (
-                            <p className='text-xl text-gray-500'>No posts found.</p>
-                        )
-                    }
-
-                    {
-                        loading && (
-                            <p className='text-gray-500'>Loading...</p>
-                        )
-                    }
-
-                    {!loading &&
-                        posts &&
-                        posts.map((post) => (
-                            <PostCard key={post._id} post={post} />
-                        ))
-                    }
-
-                    {
-                        showMore && <button onClick={handleShowMore} className='text-teal-500 text-lg hover:underline p-7 w-full'></button>
-                    }
-                </div>
+            <div className='flex items-center gap-2'>
+              <label className='font-semibold'>Sort:</label>
+              <Select onChange={handleChange} value={sideBarData.sort} id='sort'>
+                <option value='desc'>Latest</option>
+                <option value='asc'>Oldest</option>
+              </Select>
             </div>
+            <div className='flex items-center gap-2'>
+              <label className='font-semibold'>Category:</label>
+              <Select
+                onChange={handleChange}
+                value={sideBarData.category}
+                id='category'
+              >
+                <option value='uncategorized'>Uncategorized</option>
+                <option value='reactjs'>React.js</option>
+                <option value='nextjs'>Next.js</option>
+                <option value='javascript'>JavaScript</option>
+              </Select>
+            </div>
+            <Button type='submit' outline gradientDuoTone='purpleToPink'>
+              Apply Filters
+            </Button>
+          </form>
         </div>
+        <div className='w-full'>
+          <h1 className='text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5 '>
+            Posts results:
+          </h1>
+          <div className='p-7 flex flex-wrap gap-4'>
+            {!loading && posts.length === 0 && (
+              <p className='text-xl text-gray-500'>No posts found.</p>
+            )}
+            {loading && <p className='text-xl text-gray-500'>Loading...</p>}
+            {!loading &&
+              posts &&
+              posts.map((post) => <PostCard key={post._id} post={post} />)}
+            {showMore && (
+              <button
+                onClick={handleShowMore}
+                className='text-teal-500 text-lg hover:underline p-7 w-full'
+              >
+                Show More
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
     )
 }
