a980643a636c6cdce62c1a09b1bf399ab105c1d3
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2025-01-09 15:57:48 +0200

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,16 @@
-const flowbite = require("flowbite-react/tailwind");
+import { content as _content } from "flowbite-react/tailwind";
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class', // Enable dark mode with a class
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    flowbite.content(), // Include Flowbite content
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    flowbite.content(),
-    require('tailwind-scrollbar'),
-  ],
-
-}
+export const darkMode = 'class';
+export const content = [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}',
+  _content(), // Include Flowbite content
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [
+  _content(),
+  require('tailwind-scrollbar'),
+];
 
